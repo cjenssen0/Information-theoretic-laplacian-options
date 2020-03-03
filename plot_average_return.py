1e0003e242364550cc989d8de82813e789183ae8
Jonas <jonas.n.myhre@uit.no>
2020-03-03 09:25:22 +0100

    Add args to plotting function.

--- plot_average_return.py
+++ plot_average_return.py
@@ -1,11 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('file', type=str,
+                    help='Name of average return file to load.')
+
+args = parser.parse_args()
 
 if __name__ == "__main__":
-   results = np.load('data_files/average_return.npy')
+   # results = np.load('data_files/average_return.npy')
    # results = np.load('data_files/average_return_K.npy')
-   # results = np.load('data_files/average_return_OG.npy')
+   results = np.load(args.file)
 
    plt.show()
    x_legend = range(len(results[0][:]))
@@ -13,7 +21,7 @@ if __name__ == "__main__":
    graph_agent_2, = plt.plot(x_legend, results[2][:], label="2 option")
    graph_agent_4, = plt.plot(x_legend, results[4][:], label="4 options")
    graph_agent_8, = plt.plot(x_legend, results[8][:], label="8 options")
-   graph_agent_64, = plt.plot(x_legend, results[64][:], label="64 options")
+   graph_agent_64, = plt.plot(x_legend, results[30][:], label="64 options")
 #    graph_agent_128, = plt.plot(x_legend, results[128][:], label="128 options")
 #    graph_agent_200, = plt.plot(x_legend, results[200][:], label="200 options")
 
