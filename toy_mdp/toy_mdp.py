790cc5391b95ce7b653af6c341f9425cbc590b66
Jonas <jonas.n.myhre@uit.no>
2020-06-04 15:39:45 +0200

    Update toy example.

--- toy_mdp/toy_mdp.py
+++ toy_mdp/toy_mdp.py
@@ -2,45 +2,79 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.spatial.distance import pdist,squareform, cdist
 from sklearn.neighbors import kneighbors_graph
-from scipy import linalg
+# from scipy import linalg
+
+def create_env(n1=100, n2=100, sigma1=1, sigma2=1, savestring='environment_txt'):
 # two Gaussian components
-n = 100
+    # n1 = 100
+    # n2 = 100
+    # sigma1 = 1
+    # sigma2 = 1
 
-x1 = np.random.multivariate_normal([0,0], np.eye(2), n)
-x2 = np.random.multivariate_normal([10, 0], np.eye(2), n)
+    x1 = np.round(np.random.multivariate_normal([0,0], sigma1 * np.eye(2), n1))
+    x2 = np.round(np.random.multivariate_normal([10, 0], sigma2 * np.eye(2), n2))
 
-d12 = cdist(x1, x2)
+    # d12 = cdist(x1, x2)
 
-min12 = np.argmin(d12)
+    # min12 = np.argmin(d12)
 
-c12_1, c12_2 = np.unravel_index(min12, [n, n])
+    # c12_1, c12_2 = np.unravel_index(min12, [n1, n2])
 
-plt.figure()
+    # plt.figure()
 
-# Merging the Gaussians to a data set
-x = np.concatenate([x1, x2])
-plt.scatter(x[:,0], x[:, 1])
-plt.scatter(x1[c12_1, 0], x1[c12_1, 1])
-plt.scatter(x2[c12_2, 0], x2[c12_2, 1])
-plt.show()
-
-
-# Finding closest indices in all components
-
-# x = x.T
-
-# Pairwise distance matrix
-D = squareform(pdist(x))
+    # Merging the Gaussians to a data set
+    x = np.concatenate([x1, x2])
+    # plt.scatter(x[:,0], x[:, 1])
+    # plt.scatter(x1[c12_1, 0], x1[c12_1, 1])
+    # plt.scatter(x2[c12_2, 0], x2[c12_2, 1])
+    # plt.show()
 
 
-# Creating a nn - graph
+    # Finding closest indices in all components
 
-nn = kneighbors_graph(x, 4, mode='connectivity', include_self=True)
-nn = nn.toarray()
+    # x = x.T
 
-# plt.figure(figsize=(20,20))
-# plt.scatter(x[0, :], x[1, :])
+    # Pairwise distance matrix
+    # D = squareform(pdist(x))
 
-# for i in range(400):
-    # indices = np.squeeze(np.nonzero(nn[i,:]))
-    # plt.plot([x[0, indices[0]], x[0, indices[1]]],[x[1, indices[0]], x[1, indices[1]]])
+
+    # Creating a nn - graph
+
+    # nn = kneighbors_graph(x, 4, mode='connectivity', include_self=True)
+    # nn = nn.toarray()
+
+    # plt.figure(figsize=(20,20))
+    # plt.scatter(x[0, :], x[1, :])
+
+    # for i in range(400):
+        # indices = np.squeeze(np.nonzero(nn[i,:]))
+        # plt.plot([x[0, indices[0]], x[0, indices[1]]],[x[1, indices[0]], x[1, indices[1]]])
+
+    # HACK!
+    X = x
+    X[:, 0] = X[:, 0] - min(X[:, 0])
+    X[:, 1] = X[:, 1] - min(X[:, 1])
+
+    # plt.figure()
+    # plt.scatter(X[:,0], X[:, 1])
+    # plt.scatter(X[c12_1, 0], X[c12_1, 1])
+    # plt.scatter(X[c12_2 + n2, 0], X[c12_2 + n2, 1])
+    # plt.show()
+
+    # Creating environment
+    mat = np.zeros([int(np.max(X[:, 1])), int(np.max(X[:, 0]))])
+    for i, j in X:
+        mat[int(j)-1, int(i)-1] = 1
+
+    plt.figure()
+    plt.imshow(mat)
+    plt.show()
+
+    # Save environment for importin in RLGLUE
+    np.savetxt(fname=savestring, X=mat.astype(int), fmt ='%.0f', delimiter='')
+
+
+if __name__ == '__main__':
+    create_env()
+    create_env(10, 100, 1, 1, 'environment_mixture_weight2.txt')
+    create_env(100, 100, .5, 5, 'environment_variance2.txt')
